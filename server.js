023be98e25c8981b9816412d873d0ecde52566e7
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-04-01 23:00:57 +0530

    Restructure project
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- server.js
+++ server.js
@@ -5,55 +5,41 @@
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
-const index = require('./routes/index')
 const path = require('path')
-
-app.set('views', __dirname +'/views');
-app.set('view engine', 'hbs');
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.post('/addhostel', function (req, res) {
 
-app.use('/', index.router);
-app.use('/',express.static(__dirname + '/public'));
-
-app.listen(8888, () => {
-  console.log("Server Listening on port 8888");
-})
-
-app.post('/addhostel',function (req,res) {
-
-    console.log(req.body.hid);
-    console.log(req.body.name);
-    console.log(req.body.capacity);
+  console.log(req.body.hid);
+  console.log(req.body.name);
+  console.log(req.body.capacity);
 
 })
 
-app.post('/addapplicant',function (req,res) {
+app.post('/addapplicant', function (req, res) {
 
-    console.log(req.body.name);
-    console.log(req.body.rno);
-
-})
-
-app.post('/addapplication',function (req,res) {
-
-    console.log(req.body.date);
-    console.log(req.body.status);
-    console.log(req.body.rno);
-    console.log(req.body.aid);
+  console.log(req.body.name);
+  console.log(req.body.rno);
 
 })
 
-app.post('/addstaff',function (req,res) {
+app.post('/addapplication', function (req, res) {
 
-    console.log(req.body.hid);
-    console.log(req.body.sid);
-    console.log(req.body.tenure);
-    console.log(req.body.salary);
+  console.log(req.body.date);
+  console.log(req.body.status);
+  console.log(req.body.rno);
+  console.log(req.body.aid);
+
+})
+
+app.post('/addstaff', function (req, res) {
+
+  console.log(req.body.hid);
+  console.log(req.body.sid);
+  console.log(req.body.tenure);
+  console.log(req.body.salary);
 
 })
 
