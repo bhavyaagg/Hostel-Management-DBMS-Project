10002cc51120e89599a0fa553aa9d4d5195a076f
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-04-08 23:23:23 +0530

    Add check contraints for staff model
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- db/queries/staff.js
+++ db/queries/staff.js
@@ -3,10 +3,10 @@
  */
 
 const createTable = `CREATE TABLE IF NOT EXISTS staff(
-    sid int,
-    hid int,
+    sid int check(sid>0),
     tenure int,
-    salary int,
+    salary int check(salary>0),
+    hid int, 
     PRIMARY KEY (sid),
     FOREIGN KEY (hid) references hostel(hid)  
   );
