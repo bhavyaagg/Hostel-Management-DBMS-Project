1422d5aab5d604f082d2b1bc69f1a91db023300b
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-04-09 10:55:26 +0530

    Modularize models
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- db/models.js
+++ db/models.js
@@ -4,26 +4,13 @@
 
 const Sequelize = require('sequelize');
 const dbConfig = require('./dbConfig');
-const queries = require('./queries/index');
+const queries = require('./queries');
 
 const db = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.DIALECT,
   port: dbConfig.PORT
 });
-
-db.query(queries.hostel.createTable).then((data) => {
-  db.query(queries.applicant.createTable).then((data) => {
-    db.query(queries.application.createTable).then((data) => {
-      db.query(queries.staff.createTable).then((data) => {
-
-      })
-    })
-  })
-
-
-});
-
 
 db.sync({force: true}).then(() => {
   console.log("Database Configured");
