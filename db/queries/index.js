e2f6cc504ec707cddb3bffda2f81132d20e0eda8
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-04-15 23:50:20 +0530

    Update require statements for queries
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- db/queries/index.js
+++ db/queries/index.js
@@ -1,10 +1,12 @@
 /**
  * Created by bhavyaagg on 02/04/18.
  */
+const fs = require('fs');
+const files = fs.readdirSync(__dirname)
 
-module.exports = {
-  application: require('./application'),
-  applicant: require('./applicant'),
-  hostel: require('./hostel'),
-  staff: require('./staff')
-}
+module.exports = files.reduce((prev, filename) => {
+  if (filename.split('.')[0] !== 'index') {
+    prev[filename.split('.')[0]] = require(`./${filename}`)
+  }
+  return prev
+}, {})
