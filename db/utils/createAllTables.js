2af7130834fa937538f529d9f5df43acd5a33709
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-04-18 20:03:19 +0530

    Update createAllTablesScript
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- db/utils/createAllTables.js
+++ db/utils/createAllTables.js
@@ -12,6 +12,7 @@ db.query(queries.hostel.createTable).then((data) => {
     db.query(queries.application.createTable).then((data) => {
       db.query(queries.staff.createTable).then((data) => {
         console.log("All tables created");
+        process.exit();
       })
     })
   })
