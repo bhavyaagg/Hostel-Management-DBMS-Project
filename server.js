00a4c66d12ca03be83d899ea5b57a800d8c44486
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-04-25 10:23:47 +0530

    Add passport
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- server.js
+++ server.js
@@ -3,14 +3,35 @@
  */
 
 const express = require('express');
+const session = require('express-session')
 const app = express();
-const db = require('./db/models');
+const passport = require('./auth/mypassport')
+
+const db = require('./db/models').db;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+  secret: 'something that should not be shared',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/api', require('./routes/index'));
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
+
+app.post('/', passport.authenticate('local', {failureRedirect: '/signin'}), (req, res) => {
+  res.redirect('/');
+})
+
 
 app.listen(8888, () => {
   console.log("Server Listening on port 8888");
