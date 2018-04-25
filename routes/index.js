8a314f9fc24070ee0a6360098a39be84ddcf5174
Astitwa Saxena <astu9880@Astitwas-MacBook-Pro.local>
2018-04-25 12:36:29 +0530

    Astu ka pehla commit

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,6 @@ const route = express.Router();
 route.use('/applicant', require('./applicant'));
 route.use('/application', require('./application'));
 route.use('/hostel', require('./hostel'));
-route.use('/staff', require('./staff'));
+route.use('/staff1', require('./staff'));
 
 module.exports = route;
