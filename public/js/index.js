d3de63857cec53b6cbda67c11034054565aedcf1
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-04-25 10:22:28 +0530

    Update frontend add login and register
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by bhavyaagg on 25/04/18.
+ */
+$(document).ready(() => {
+  $('#showFormsBtn').click(() => {
+    $('#showForms').css('display','none');
+    $('#forms').css('display', 'block');
+  })
+})
